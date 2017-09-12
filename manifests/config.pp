badf32a703849ca4ee5f62c7868280470fee6b0e
Paul Musso <paul@musso.io>
2017-09-12 15:57:02 +0000

    update install, config and system

---
+++ manifests/config.pp
@@ -0,0 +1,22 @@
+class nginx::config(
+  String $ensure                = $::nginx::config_ensure,
+  String $config_dir            = $::nginx::config_dir,
+  String $mode                  = $::nginx::config_mode,
+  String $owner                 = $::nginx::config_owner,
+  String $group                 = $::nginx::config_group,
+  String $confd                 = $::nginx::config_confd,
+  String $log_dir               = $::nginx::config_log_dir,
+  String $pid_file              = $::nginx::config_pid_file,
+  Optional[String] $vdir_enable = $::nginx::config_vdir_enable,
+  String $process_user          = $::nginx::config_process_user,
+  String $docroot               = $::nginx::docroot,
+) {
+  file { 'nginx_conf':
+    ensure => $ensure,
+    path   => "${config_dif}/nginx.conf",
+    mode   => $mode,
+    owner  => $owner,
+    group  => $group,
+    content => template("${module_name}/conf.d/nginx.conf/erb"),
+  }
+}
