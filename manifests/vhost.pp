22711a9b8eaf8af2d88ba8f4a1a4ffcb64dd8e96
Paul Musso <paul@musso.io>
2017-09-12 19:24:53 +0000

    add vhost define type

---
+++ manifests/vhost.pp
@@ -0,0 +1,20 @@
+define nginx::vhost(
+  Integer $port                        = 80,
+  String $priority                     = '50',
+  String $server_aliases               = '',
+  Boolean $enable                      = true,
+  String $owner                        = $::nginx::config_owner,
+  String $group                        = $::nginx::config_group,
+  String $mode                         = $::nginx::config_mode,
+  Optional[String] $config_vdir_enable = $::nginx::config_vdir_enable,
+  String $log_dir                      = $::nginx::config_log_dir,
+  String $vhost_dir                    = $::nginx::vhost_dir,
+) {
+  file { "${vhost_dir}/${priority}-${name}.conf":
+    content => template("${module_name}/vhost/vhost.conf.erb"),
+    mode    => $mode,
+    owner   => $owner,
+    group   => $group,
+    notify  => Service['nginx_service'],
+  }
+}
